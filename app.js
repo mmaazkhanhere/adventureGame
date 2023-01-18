72a3943a8436e03382a6e19ec7dd09de52bae3d6
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-01-18 14:52:59 +0500

    adding strike function

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 #! /usr/bin/env node
+import inquirer from "inquirer";
 import chalkAnimation from "chalk-animation";
+var playerHP = 100;
+var enemyHP = 100;
+var healthRegain = 3;
+let enemiesList = ['Archer', 'Samurai', 'Ghoul', 'Bomberman'];
+var enemyToFight = enemiesList[Math.ceil(Math.random() * 4)];
 function animationStop() {
     return new Promise((res) => {
         setTimeout(res, 2500);
@@ -12,4 +18,82 @@ async function animationDisplay() {
     await animationStop();
     secondTitle.stop();
 }
-animationDisplay();
+async function userOptions() {
+    console.log(`${enemyToFight} has arrived at the scene!`);
+    let userOptions = await inquirer.prompt([{
+            name: 'userOptions',
+            type: 'list',
+            message: 'Choose an action!',
+            choices: ['Strike', 'Regain Health', 'Retreat']
+        }]);
+    switch (userOptions.userOptions) {
+        case 'Strike':
+            await strike();
+            break;
+        case 'Regain Health':
+            await healthBoost();
+            break;
+    }
+}
+async function strike() {
+    if (enemyToFight == 'Archer') {
+        let playerDamage = Math.ceil(Math.random() * 20);
+        let enemyDamage = Math.ceil(Math.random() * 10);
+        playerHP = playerHP - enemyDamage;
+        enemyHP = playerHP - playerDamage;
+        if (enemyHP < 0) {
+            console.log(`You have defeated ${enemyToFight}Player Health: ${playerHP}\nHealth Regain Drink:${healthRegain}`);
+        }
+        else {
+            console.log(`Player Health: ${playerHP}\nEnemy Health:${enemyHP}`);
+        }
+    }
+    else if (enemyToFight == 'Ghoul') {
+        let playerDamage = Math.ceil(Math.random() * 19);
+        let enemyDamage = Math.ceil(Math.random() * 12);
+        playerHP = playerHP - enemyDamage;
+        enemyHP = playerHP - playerDamage;
+        if (enemyHP < 0) {
+            console.log(`You have defeated ${enemyToFight}Player Health: ${playerHP}\nHealth Regain Drink:${healthRegain}`);
+        }
+        else {
+            console.log(`Player Health: ${playerHP}\nEnemy Health:${enemyHP}`);
+        }
+    }
+    else if (enemyToFight == 'Bomberman') {
+        let playerDamage = Math.ceil(Math.random() * 18);
+        let enemyDamage = Math.ceil(Math.random() * 14);
+        playerHP = playerHP - enemyDamage;
+        enemyHP = playerHP - playerDamage;
+        if (enemyHP < 0) {
+            console.log(`You have defeated ${enemyToFight}Player Health: ${playerHP}\nHealth Regain Drink:${healthRegain}`);
+        }
+        else {
+            console.log(`Player Health: ${playerHP}\nEnemy Health:${enemyHP}`);
+        }
+    }
+    else if (enemyToFight == 'Samurai') {
+        let playerDamage = Math.ceil(Math.random() * 17);
+        let enemyDamage = Math.ceil(Math.random() * 16);
+        playerHP = playerHP - enemyDamage;
+        enemyHP = playerHP - playerDamage;
+        if (enemyHP < 0) {
+            console.log(`You have defeated ${enemyToFight}Player Health: ${playerHP}\nHealth Regain Drink:${healthRegain}`);
+        }
+        else {
+            console.log(`Player Health: ${playerHP}\nEnemy Health:${enemyHP}`);
+        }
+    }
+}
+async function healthBoost() {
+    if (healthRegain == 0) {
+        console.log(`Cannot add anymore health`);
+    }
+    else {
+        --healthRegain;
+        playerHP += 45;
+        console.log(`Player Health: ${playerHP}\nHealth Regain Drink left: ${healthRegain}`);
+    }
+}
+//animationDisplay();
+userOptions();
--- app.ts
+++ app.ts
@@ -6,6 +6,10 @@ import chalkAnimation from "chalk-animation"
 
 var playerHP:number=100;
 var enemyHP:number=100;
+var healthRegain=3;
+
+let enemiesList:string[]=['Archer','Samurai','Ghoul','Bomberman'];
+var enemyToFight=enemiesList[Math.ceil(Math.random()*4)]
 
 function animationStop(){
     return new Promise((res)=>{
@@ -22,27 +26,84 @@ async function animationDisplay(){
 }
 
 async function userOptions(){
-    do{
-        var enemiesList:string[]=['Archer','Samurai','Ghoul','Bomberman'];
-
-        console.log(`${enemiesList[Math.ceil(Math.random()*4)]}`)
-        let userOptions= await inquirer.prompt([{
-            name:'userOptions',
-            type:'list',
-            message:'Choose an action!',
-            choices:['Strike','Regain Health','Retreat']
-        }])
-        // switch(userOptions.userOptions){
-        //     case 'Strike':
-
-        // }
-        
-    }while()
+    console.log(`${enemyToFight} has arrived at the scene!`)
+    let userOptions= await inquirer.prompt([{
+        name:'userOptions',
+        type:'list',
+        message:'Choose an action!',
+        choices:['Strike','Regain Health','Retreat']
+    }]);
+    switch(userOptions.userOptions){
+        case 'Strike':
+            await strike();
+            break;
+        case 'Regain Health':
+            await healthBoost();
+            break;
+        }
 }
 
 async function strike(){
-    let userDamage:number=Math.ceil(Math.random()*100);
-    let enemyDamage:
+    if(enemyToFight=='Archer'){
+        let playerDamage:number=Math.ceil(Math.random()*20);
+        let enemyDamage:number=Math.ceil(Math.random()*10);
+        playerHP=playerHP-enemyDamage;
+        enemyHP=playerHP-playerDamage;
+        if(enemyHP<0){
+            console.log(`You have defeated ${enemyToFight}Player Health: ${playerHP}\nHealth Regain Drink:${healthRegain}`)
+        }
+        else{
+            console.log(`Player Health: ${playerHP}\nEnemy Health:${enemyHP}`)
+        }
+    }
+    else if(enemyToFight=='Ghoul'){
+        let playerDamage:number=Math.ceil(Math.random()*19);
+        let enemyDamage:number=Math.ceil(Math.random()*12);
+        playerHP=playerHP-enemyDamage;
+        enemyHP=playerHP-playerDamage;
+        if(enemyHP<0){
+            console.log(`You have defeated ${enemyToFight}Player Health: ${playerHP}\nHealth Regain Drink:${healthRegain}`)
+        }
+        else{
+            console.log(`Player Health: ${playerHP}\nEnemy Health:${enemyHP}`)
+        }
+    }
+    else if(enemyToFight=='Bomberman'){
+        let playerDamage:number=Math.ceil(Math.random()*18);
+        let enemyDamage:number=Math.ceil(Math.random()*14);
+        playerHP=playerHP-enemyDamage;
+        enemyHP=playerHP-playerDamage;
+        if(enemyHP<0){
+            console.log(`You have defeated ${enemyToFight}Player Health: ${playerHP}\nHealth Regain Drink:${healthRegain}`)
+        }
+        else{
+            console.log(`Player Health: ${playerHP}\nEnemy Health:${enemyHP}`)
+        }
+    }
+    else if(enemyToFight=='Samurai'){
+        let playerDamage:number=Math.ceil(Math.random()*17);
+        let enemyDamage:number=Math.ceil(Math.random()*16);
+        playerHP=playerHP-enemyDamage;
+        enemyHP=playerHP-playerDamage;
+        if(enemyHP<0){
+            console.log(`You have defeated ${enemyToFight}Player Health: ${playerHP}\nHealth Regain Drink:${healthRegain}`)
+        }
+        else{
+            console.log(`Player Health: ${playerHP}\nEnemy Health:${enemyHP}`)
+        }
+    }
 }
 
-animationDisplay();
+async function healthBoost(){
+    if(healthRegain==0){
+        console.log(`Cannot add anymore health`)
+    }
+    else{
+        --healthRegain;
+        playerHP += 45
+        console.log(`Player Health: ${playerHP}\nHealth Regain Drink left: ${healthRegain}`)
+    }
+}
+
+//animationDisplay();
+userOptions();
